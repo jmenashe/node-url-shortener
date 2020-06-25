0afd8f91ab86afdc7572bf244689428cfb0c14bf
Jacob Menashe <jmenashe@cs.utexas.edu>
2020-06-25 21:09:02 +0000

    Added support for urls without protocols.

--- public/js/nus.js
+++ public/js/nus.js
@@ -25,7 +25,7 @@
   };
 
   _nus.prototype.check = function (s) {
-    var regexp = /^(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+    var regexp = /^(?:(ftp|http|https):\/\/)?(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
     return regexp.test(s);
   };
 
